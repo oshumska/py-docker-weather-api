8e3db45794d91cdd0da2622438e8e394f689f7f4
Olha Shumska <olgashumska65@gmail.com>
2025-08-24 14:19:59 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,30 +2,51 @@ import os
 import requests
 
 BASE_URL = "https://api.weatherapi.com/v1/current.json"
+CITY = "Paris"
+PARAM_KEY = "?key="
+PARAM_Q = "&q="
 
 
 def get_weather() -> None:
     # write your code here
     api_key = os.environ.get("API_KEY")
-    paris = "Paris"
     if api_key:
-        url = BASE_URL + "?key=" + api_key + "&q=" + paris
-        print(f"Performing request for Weather API for city {paris}")
-        response = requests.get(url)
-        if response.ok:
-            response = response.json()
-            city = response["location"]["name"]
-            country = response["location"]["country"]
-            date_time_local = response["location"]["localtime"]
-            temp_c = response["current"]["temp_c"]
-            condition = response["current"]["condition"]["text"]
+        url = BASE_URL + PARAM_KEY + api_key + PARAM_Q + CITY
+        print(f"Performing request for Weather API for city {CITY}")
+        try:
+            response = requests.get(url, timeout=30)
+            data = response.json()
+            location_data = data.get("location")
+            current_data = data.get("current")
+            city = location_data.get("name")
+            if not city:
+                city = "Paris"
+            country = location_data.get("country")
+            if not country:
+                country = "France"
+            date_time_local = location_data.get("localtime")
+            if not date_time_local:
+                date_time_local = "time to have a break"
+            temp_c = current_data.get("temp_c")
+            if not temp_c:
+                temp_c = "N/A"
+            condition = current_data.get("condition").get("text")
+            if not condition:
+                condition = "better check the window"
 
             print(f"{city}/{country}"
                   f" {date_time_local} "
                   f"Weather: {temp_c} "
                   f"Celsius, {condition}")
-        else:
-            print(response.json()["error"]["message"])
+        except requests.exceptions.Timeout:
+            print("The request timed out.")
+        except requests.exceptions.HTTPError as e:
+            if e.response.status_code in (401, 403):
+                print("API Key is invalid")
+            else:
+                print(f"Error: {e}")
+        except requests.exceptions.RequestException as e:
+            print(f"Error: {e}")
 
     else:
         print("API Key not set")
